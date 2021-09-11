2ccbc11d198e824896073241a61274ad1e0fa0b4
auorra <auora2020@protonmail.com>
2021-09-10 18:26:29 -0700

    new commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //jshint esversion: 6
 
+const player = document.querySelector('.player');
 const video = document.querySelector('video');
 const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
@@ -112,6 +113,39 @@ function changeSpeed() {
 	video.playbackRate = speed.value;
 }
 
+//FullScreen
+/* View in fullscreen */
+function openFullscreen(elem) {
+	if (elem.requestFullscreen) {
+	  elem.requestFullscreen();
+	} else if (elem.webkitRequestFullscreen) { /* Safari */
+	  elem.webkitRequestFullscreen();
+	} else if (elem.msRequestFullscreen) { /* IE11 */
+	  elem.msRequestFullscreen();
+	}
+  }
+  
+  /* Close fullscreen */
+  function closeFullscreen() {
+	if (document.exitFullscreen) {
+	  document.exitFullscreen();
+	} else if (document.webkitExitFullscreen) { /* Safari */
+	  document.webkitExitFullscreen();
+	} else if (document.msExitFullscreen) { /* IE11 */
+	  document.msExitFullscreen();
+	}
+  }
+
+let fullscreen = false;
+//Toggle Fullscreen
+function toggleFullscreen() {
+	if (!fullscreen) {
+		openFullscreen(player);
+	} else {
+		closeFullscreen();
+	}
+	fullscreen = !fullscreen;
+}
 
 //Event Listeners
 playBtn.addEventListener('click', togglePlay);
@@ -122,3 +156,4 @@ progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
 speed.addEventListener('change', changeSpeed);
+fullscreenBtn.addEventListener('click', toggleFullscreen);
